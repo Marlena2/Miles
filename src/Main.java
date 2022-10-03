00dfe950a1e0eb7834555393cbbcea5b65b8beed
Elena Martynova <martynova.elena@gmail.com>
2022-10-03 18:33:42 +0300

    update application

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketCost = 2400; // рублей
-        int bonus = 20; // рублей
-        int miles = ticketCost / bonus;
-
-        System.out.println("Расчет количества бонусных милей");
-        System.out.println("За каждые 20 р., потраченные на билет - 1 миля");
-        System.out.println("Стоимость билета: " + (ticketCost) + " р.");
-        System.out.println("Начислено: " + miles + " бонусных миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Бонус: " + miles);
 
 
     }
